9e380c652416a93cd3355082ae67e2b852113fc8
Mark Richman <mark@markrichman.com>
2013-09-23 10:05:22 -0400

    *** Production mode for email recipient ***
    Production mode for email recipient

--- mom.py
+++ mom.py
@@ -219,7 +219,7 @@ class Order(object):
             self.cust_num = row.get('CUSTNUM', '')
             self.first_name = row['FIRSTNAME']
             self.last_name = row.get('LASTNAME', '')
-            self.email = 'mark.richman@nutrihealth.com'  # row['EMAIL']
+            self.email = row.get('EMAIL', 'mark.richman@nutrihealth.com')
             self.expect_ship = row.get('SHIP_DATE', date.max)
             if self.expect_ship == date.max:
                 self.expect_ship = row.get('NEXT_SHIP', date.max)
